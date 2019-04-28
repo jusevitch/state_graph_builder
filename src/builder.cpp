e69d1c59fdd747aeb7f6a4ebaf93c34afafe4aa1
James Usevitch <usevitch@umich.edu>
2019-04-27 22:48:51 -0400

    Merged into master

--- src/builder.cpp
+++ src/builder.cpp
@@ -24,7 +24,8 @@ class Builder
 
   int n;
   int id;// 1 for ugv 0 for uav
-  bool env;
+  // bool env;
+  int gazebo;
 
   ros::Subscriber uav_msgs;
   std::vector<ros::Subscriber> uav_subs;
@@ -49,15 +50,18 @@ class Builder
 
 };
 void Builder::ugv1_subCallback(const nav_msgs::Odometry::ConstPtr& msgs, const int list_idx){
-  ugv_list1[list_idx]=msgs->pose.pose;
+  ugvout_list[list_idx].pose=msgs->pose.pose;
   
 }
 void Builder::ugvout_subCallback(const geometry_msgs::PoseStamped::ConstPtr& msgs, const int list_idx){
+  ROS_INFO("ugvout_list size: %d,\n list_idx: %d", ugvout_list.size(), list_idx);
   ugvout_list[list_idx].pose=msgs->pose;
+  // ROS_INFO(" FOOBAR index: %d, pose.position.x: %lf", list_idx, ugvout_list[list_idx].pose.position.x);
+
   
 }
 void Builder::ugv2_subCallback(const geometry_msgs::Twist::ConstPtr& msgs, const int list_idx){
-  ROS_INFO("I heard: [%lf]", msgs->linear.x);
+  // ROS_INFO("I heard: [%lf]", msgs->linear.x);
   ugv_list2[list_idx].x=msgs->linear.x;
   ugv_list2[list_idx].y=0.0;
   ugv_list2[list_idx].z=msgs->angular.z;
@@ -78,55 +82,60 @@ Builder::Builder()
 {
   
      nh_private_.param<int>("n", n, 15);
-     nh_private_.param<bool>("env",env,0);
+    //  nh_private_.param<bool>("env",env,true); // env = 1 if outdoors. env = !gazebo.
+     nh_private_.param<int>("gazebo",gazebo,0);
      uav_list.resize(n);
      ugv_list2.resize(n);
      ugv_list1.resize(n);
      ugvout_list.resize(n);
      int id = 1;
 
-     if (env==0)
-       pub=nh.advertise<state_graph_builder::posegraph>("graph", 50);
+
+
+     if (gazebo == 1)
+       pub=nh.advertise<state_graph_builder::posestampedgraph>("graph", 50);
      else
        pub=nh.advertise<state_graph_builder::posestampedgraph>("graph",50);
 
      ROS_INFO("Switch is True. Ready to record data %d", n);
-     for (int i=1; i < n+1; i++){
-       //std::string sub_topic = "/uav" + std::to_string(i) + "/ground_truth/position";
-       //uav_subs.push_back( nh.subscribe<geometry_msgs::PointStamped>(sub_topic, 10, boost::bind(&Builder::uav_subCallback,this,_1, i)) );
 
-       
-       //std::string sub_topic2 = "/ugv" + std::to_string(i) + "/cmd_vel_mux/input/teleop";
-      
-       //ugv_sub2.push_back( nh.subscribe<geometry_msgs::Twist>(sub_topic2, 10, boost::bind(&Builder::ugv2_subCallback,this,_1, i-1)) );
-       if (env==0){
-	 std::string sub_topic3 = "/ugv" + std::to_string(i) + "/odom";
-	 ugv_sub1.push_back( nh.subscribe<nav_msgs::Odometry>(sub_topic3, 10, boost::bind(&Builder::ugv1_subCallback,this,_1, i-1)) );
+	if(gazebo == 1) {
+     	for (int i=1; i < n+1; i++){
+			std::string sub_topic3 = "/R" + std::to_string(i) + "/odom";
+			ugv_sub1.push_back( nh.subscribe<nav_msgs::Odometry>(sub_topic3, 10, boost::bind(&Builder::ugv1_subCallback,this,_1, i-1)) );
        }
-       else{
-	 std::string sub_topic3 = "/R" + std::to_string(i);
-	 ugv_subout.push_back( nh.subscribe<geometry_msgs::PoseStamped>(sub_topic3, 10, boost::bind(&Builder::ugvout_subCallback,this,_1, i-1)) );
+	}
+	else{
+		std::vector<int> rovers = {4,5,6};
+		for(int i=0; i<= rovers.size(); i++) {
+		 	std::string sub_topic3 = "/R" + std::to_string(rovers[i]);
+		 	ugv_subout.push_back( nh.subscribe<geometry_msgs::PoseStamped>(sub_topic3, 10, boost::bind(&Builder::ugvout_subCallback,this,_1, i)) );
+       ROS_INFO("This worked for agent %d", rovers[i]);
        }
      }
+
+
+
      ROS_INFO("All publisher initialized");
+     ROS_INFO("gazebo: %d", gazebo);
 
      ros::Rate rate(100);
      uint time_count=0;
 
        while(ros::ok()) {
 
-	 if (env==0){
-       	 posemsg.poses.clear();
-       	 posemsg.id.clear();
-       	 posemsg.time=time_count;
+	 if (gazebo == 1){
+       	 posetmsg.poses.clear();
+       	 posetmsg.id.clear();
+       	 posetmsg.time=time_count;
        	 for (int i=0; i<n; i++){
        	   //msg.points.push_back(uav_list[i]);
        	   //msg.points.push_back(ugv_list2[i]);
-	   posemsg.poses.push_back(ugv_list1[i]);
-       	   posemsg.id.push_back(id);
+	        posetmsg.poses.push_back(ugvout_list[i]);
+       	  posetmsg.id.push_back(id);
        	 }
 
-       	 pub.publish(posemsg);
+       	 pub.publish(posetmsg);
 	 
           //Delays untill it is time to send another message
        	  time_count+=1;
